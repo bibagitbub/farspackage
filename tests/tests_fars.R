b652da3587b3eda3c323743a264ac2983e2bbf08
bibagitbub <federica.bisio@gmail.com>
2017-03-27 12:26:22 +0200

    Add files via upload

--- tests/tests_fars.R
+++ tests/tests_fars.R
@@ -1,9 +1,9 @@
 library(testthat)
 
-expect_that(fars_read(filename="accident_2014.csv.bz2"),is_a("tbl_df"))
+expect_that(farspackage::fars_read(filename="accident_2014.csv.bz2"),is_a("tbl_df"))
 
-expect_that(make_filename(year=2014),is_a("character"))
+expect_that(farspackage::make_filename(year=2014),is_a("character"))
 
-expect_that(fars_read_years(years=c(2014,2015)),is_a("list"))
+expect_that(farspackage::fars_read_years(years=c(2014,2015)),is_a("list"))
 
-expect_error(fars_map_state(state.num="STATE",year=2014))
+expect_error(farspackage::fars_map_state(state.num="STATE",year=2014))
